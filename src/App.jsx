6d3405205a4b08eb19aeaef92d1595a4f77ae86a
toantqph38960 <quavang2k4@gmail.com>
2024-08-29 15:47:10 +0700

    search

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,83 @@
-import { useState, useEffect } from 'react'
-import Banner from './components/Banner'
-import Header from './components/Header'
-import MovieList from './components/MovieList'
-import Footer from './components/Footer';
+import { useEffect } from "react";
+import Banner from "./components/Banner";
+import Header from "./components/Header";
+import MovieList from "./components/MovieList";
+import { useState } from "react";
+import MovieSearch from "./components/MovieSearch";
+import { MovieProvider } from "./context/MovieDetailContext";
 
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-  },
-};
 function App() {
-  const [movie, setMovie] = useState([]);
-  const [movieRate, setMovieRate] = useState([]);
+  const [trendingMovies, setTrendingMovies] = useState([]);
+  const [topRatedMovies, setTopRatedMovies] = useState([]);
+
+  const [searchData, setSearchData] = useState([]);
+
+  const handleSearch = async (value) => {
+    const url = `https://api.themoviedb.org/3/search/movie?query=${value}&include_adult=false&language=vi&page=1`;
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+      },
+    };
+    if (value === "") return setSearchData([]);
+    try {
+      const response = await fetch(url, options);
+      const data = await response.json();
+      setSearchData(data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
-    const fetchMovie = async () => {
+    (async function () {
+      const urls = [
+        "https://api.themoviedb.org/3/trending/movie/day?language=vi",
+        "https://api.themoviedb.org/3/movie/top_rated?language=vi",
+        // Add more URLs here...
+      ];
       const options = {
-        method: 'GET',
+        method: "GET",
         headers: {
-          accept: 'application/json',
-          Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`
-        }
+          accept: "application/json",
+          Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+        },
       };
-      const url1 = 'https://api.themoviedb.org/3/movie/popular?language=vi-US&page=1';
-      const url2 = 'https://api.themoviedb.org/3/tv/top_rated?language=vi-US&page=1';
-      const [res1, res2] = await Promise.all([
-        fetch(url1, options),
-        fetch(url2, options),
+      const fetchMovies = async (url) => {
+        return await fetch(url, options).then((response) => response.json());
+      };
 
-      ])
-      const data1 = await res1.json();
-      const data2 = await res2.json();
-      setMovie(data1.results);
-      setMovieRate(data2.results);
+      try {
+        const response = await Promise.all(urls.map(fetchMovies));
 
-    }
-    fetchMovie();
+        setTrendingMovies(response[0].results);
+        setTopRatedMovies(response[1].results);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
   }, []);
 
   return (
     <>
-      
-        <Header />
-        <Banner />
-        <div className='bg-black p-10'>
-        <MovieList title={'Phim Hot'} data={movie} />
-        <MovieList title={'Phim Đề cử'} data={movieRate} />
-      </div>
-      <Footer/>
+      <MovieProvider>
+        <div className="h-full bg-black text-white min-h-screen pb-10 relative">
+          <Header onSearch={handleSearch} />
+          <Banner />
+          {searchData.length === 0 && (
+            <MovieList title="Phim Hot" data={trendingMovies.slice(0, 10)} />
+          )}
+          {searchData.length === 0 && (
+            <MovieList title="Phim đề cử" data={topRatedMovies.slice(0, 10)} />
+          )}
+
+          {searchData.length > 0 && <MovieSearch data={searchData} />}
+        </div>
+      </MovieProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,46 @@
-const Header = () => {
-    return (
-        <div className="p-4 bg-black flex items-center justify-between">
-            <div className="flex items-center space-x-4">
-                <h1 className="text-[30px] uppercase font-bold text-red-700">Movie</h1>
-                <nav className="flex items-center space-x-4">
-                    <a href="" className="text-white">Home</a>
-                    <a href="" className="text-white">About</a>
-                    <a href="" className="text-white">Contact</a>
+import PropTypes from "prop-types";
+import { useState } from "react";
 
-                </nav>
-            </div>
-            <div className="flex items-center space-x-4">
-                <input type="text" placeholder="Search" className="p-2 text-black rounded" />
-                <button className="p-2 text-white bg-red-600 rounded">Search</button>
-            </div>
-        </div>
-    )
-}
+const Header = ({ onSearch }) => {
+  const [search, setSearch] = useState("");
+
+  return (
+    <div className="p-4 flex justify-between  fixed top-0 left-0 w-full z-[9999]  bg-black">
+      <div className="flex items-center gap-8">
+        <h1 className="text-[30px] uppercase text-red-700 font-bold">Movie</h1>
+        <nav className="hidden md:flex items-center space-x-5">
+          <a href="#" className="hover:text-red-700">
+            Home
+          </a>
+          <a href="#" className="hover:text-red-700">
+            About
+          </a>
+          <a href="#" className="hover:text-red-700">
+            Contact
+          </a>
+        </nav>
+      </div>
+      <div className="flex items-center space-x-5">
+        <input
+          type="text"
+          placeholder="Search"
+          className="border border-gray-300 p-2 text-black"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button
+          className="bg-red-700 text-white px-3 py-1 rounded-lg"
+          onClick={() => onSearch(search)}
+        >
+          Search
+        </button>
+      </div>
+    </div>
+  );
+};
+
+Header.propTypes = {
+  onSearch: PropTypes.func.isRequired,
+};
+
 export default Header;
